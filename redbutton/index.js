c080642df425a6f4702b683cf73b9ce3c5cdedf0
Victor Fernandes <vf779080@gmail.com>
2023-04-05 20:18:48 -0300

    atualizar

---
+++ redbutton/index.js
@@ -0,0 +1,49 @@
+// resgatar o valor do mapa
+// resgatar o valor do select
+// caso não resgate nenhum dos valores, criar um mensagem que nao foi registrado
+//caso efetuado o resgate, imprirmir este dado na tela
+//pegar estes dados e enviar os respectivos dados para armazenar os valores resgatados
+//inserir os dados atualizados em outra página
+    
+    let r = null
+    let c = null
+
+    function resgatarValorMapa(regiao){
+        r = regiao
+            switch(r){
+                case 'o':
+                    return r = 'Zona Oeste'
+                case 'no':
+                    return r = 'Zona Nordeste'
+                case 'c':
+                    return r = 'Zona Central'
+                case 'sd':
+                    return r = 'Zona Sudeste'
+                case 's':
+                    return r = 'Zona Sul'
+                case 'l':
+                    return r = 'Zona leste' 
+            }
+    }
+
+    function resgatarValorSelect(){
+        let select = document.querySelector('#select').value;
+        return select
+    }
+
+    r = resgatarValorMapa();
+    
+
+    function enviar(){
+        let output = document.querySelector('#output');
+        c = resgatarValorSelect()
+        if(c === "0" || r === null || r === undefined){
+            alert('ERRO')
+        }else{
+            alert(`O registro: ${r} / ocorrência: ${c}, foi cadastrada com sucesso !`)
+        }
+    }
+
+
+
+
